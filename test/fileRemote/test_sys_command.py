24562382868d5bf0ac2e72e5ec625244fd089c95
Artic42 <main@artic42.com>
2025-07-17 16:51:04 +0200

    Checking reboot file is created and deleted

--- test/fileRemote/test_sys_command.py
+++ test/fileRemote/test_sys_command.py
@@ -1,6 +1,7 @@
 from articutils.fileRemote import sys_command
 from articutils.fileRemote import config
 from unittest.mock import patch
+from articlib.articFileUtils import fileExists
 
 TEST_CONFIG_PATH = "test/files/fileRemoteConfig.yaml"
 
@@ -10,5 +11,7 @@ def test_reboot_command_executes(mock_get):
     conf = config.Config()
     conf.update_config(TEST_CONFIG_PATH)
     open(conf.monitoring_path() + "/REBOOT", 'w')
+    assert fileExists(conf.monitoring_path() + "/REBOOT")
     sys_command.monitor()
+    assert not fileExists(conf.monitoring_path() + "/REBOOT")
     mock_get.assert_called_once_with("sudo reboot")
