313dc8948b267bb251190952a62bdd7c435b7e0b
Artic42 <main@artic42.com>
2025-07-17 19:27:12 +0200

    Update command

--- articutils/fileRemote/sys_command.py
+++ articutils/fileRemote/sys_command.py
@@ -5,11 +5,21 @@ from articlib.articFileUtils import fileExists
 from articlib.articFileUtils import deleteFile
 
 conf = config.Config()
+REBOOT_FILE_PATH = conf.monitoring_path() + "/REBOOT"
+UPDATE_FILE_PATH = conf.monitoring_path() + "/UPDATE"
 
 def monitor():
-    if fileExists(conf.monitoring_path() + "/REBOOT"):
-        deleteFile(conf.monitoring_path() + "/REBOOT")
+    if fileExists(REBOOT_FILE_PATH):
+        deleteFile(REBOOT_FILE_PATH)
         reboot()
+    if fileExists(UPDATE_FILE_PATH):
+        deleteFile(UPDATE_FILE_PATH)
+        update()
+
 
 def reboot():
     os.system("sudo reboot")
+
+
+def update():
+    os.system("sudo apt update && sudo apt upgrade -y")
--- test/fileRemote/test_sys_command.py
+++ test/fileRemote/test_sys_command.py
@@ -10,8 +10,19 @@ TEST_CONFIG_PATH = "test/files/fileRemoteConfig.yaml"
 def test_reboot_command_executes(mock_get):
     conf = config.Config()
     conf.update_config(TEST_CONFIG_PATH)
-    open(conf.monitoring_path() + "/REBOOT", 'w')
-    assert fileExists(conf.monitoring_path() + "/REBOOT")
+    open(sys_command.REBOOT_FILE_PATH, 'w')
+    assert fileExists(sys_command.REBOOT_FILE_PATH)
     sys_command.monitor()
-    assert not fileExists(conf.monitoring_path() + "/REBOOT")
+    assert not fileExists(sys_command.REBOOT_FILE_PATH)
     mock_get.assert_called_once_with("sudo reboot")
+
+
+@patch('os.system')
+def test_update_command_executes(mock_get):
+    conf = config.Config()
+    conf.update_config(TEST_CONFIG_PATH)
+    open(sys_command.UPDATE_FILE_PATH, 'w')
+    assert fileExists(sys_command.UPDATE_FILE_PATH)
+    sys_command.monitor()
+    assert not fileExists(sys_command.UPDATE_FILE_PATH)
+    mock_get.assert_called_once_with("sudo apt update && sudo apt upgrade -y")
