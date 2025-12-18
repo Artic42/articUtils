0425cd9ff5b7b6baf5d4db88731170feba11a996
Artic42 <main@artic42.com>
2025-12-18 18:41:42 +0100

    Update of articlib to 0.2.7

--- articutils/fileRemote/docker_command.py
+++ articutils/fileRemote/docker_command.py
@@ -1,8 +1,10 @@
 import os
+import logging
 
 from articutils.fileRemote import config
 from articlib.articFileUtils import fileExists
 from articlib.articFileUtils import deleteFile
+from articlib.logUtils import logFile
 
 
 conf = config.Config()
@@ -10,6 +12,7 @@ UP_FILE_PATH = conf.monitoring_path() + "/UP_DOCKER_CONTAINER"
 DOWN_FILE_PATH = conf.monitoring_path() + "/DOWN_DOCKER_CONTAINER"
 CREATE_FILE_PATH = conf.monitoring_path() + "/CREATE_DOCKER_CONTAINER"
 REMOVE_FILE_PATH = conf.monitoring_path() + "/REMOVE_DOCKER_CONTAINER"
+log = logging.getLogger()
 
 
 def monitor():
@@ -28,10 +31,14 @@ def monitor():
 
 
 def upContainer():
+    log.info("Get up container into system")
+    logFile(UP_FILE_PATH)
     os.system(f"docker-compose up -d -f {UP_FILE_PATH}")
 
 
 def downContainer():
+    log.info("Get up container into system")
+    logFile(UP_FILE_PATH)
     os.system(f"docker-compose down -d -f {DOWN_FILE_PATH}")
 
 
@@ -39,6 +46,8 @@ def buildImage():
     FP = open(CREATE_FILE_PATH, "r")
     lines = FP.readlines()
     imageName = lines[0][2:]
+    log.info(f"Build image with name {imageName} with dockerfile:")
+    logFile(CREATE_FILE_PATH)
     os.system(f"docker build -t {imageName} {CREATE_FILE_PATH}")
 
 
@@ -46,4 +55,5 @@ def removeImage():
     FP = open(REMOVE_FILE_PATH, "r")
     lines = FP.readlines()
     imageName = lines[0]
+    log.info(f"Delete image with name {imageName}")
     os.system(f"docker rmi {imageName}")
